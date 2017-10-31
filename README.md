3f660076125eb6addd937910151b1d950a30d100
jjakucionis <jjakucionis@icloud.com>
2017-10-31 19:00:22 +0200

    Created from PSD file. Blog still needs update

---
+++ README.md
@@ -0,0 +1,1 @@
+This is main project from Vilnius Coding School intense bootcamp.
--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,6 @@
-// huehuehue
+$(document).ready(function() {
 
-
-// carousel on
-
-$('.carousel').carousel({
-    interval: false
-});
-
-
-// google map
-
-function initMap() {
-    var uluru = {lat: 54.687, lng: 25.279};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 7,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-  }
-
-
-//   smooth scroll
+// smooth scroll
 
 	$('.anchorlink').click(function(event){
 
@@ -35,3 +12,69 @@ function initMap() {
 
 
 
+// burger menu
+
+$('#burger-toggle').click(function() {
+  $(this).toggleClass('toggle-active');
+  $('#overlay').toggleClass('nav-active');
+});
+
+$('#overlay ul li a').click(function() {
+  $('#overlay').toggleClass('nav-active');
+  $('#burger-toggle').toggleClass('toggle-active');
+
+});
+
+// hidden img show more 
+
+$('#show-more').click(function() {
+  if ($('.hidden-img').hasClass('visible-img')) {
+    $('.hidden-img').removeClass('visible-img');
+    document.getElementById("show-more").textContent="show less";
+  } else {
+    $('.hidden-img').addClass('visible-img');   
+    $('html, body').animate({scrollTop: $("#work").offset().top}, 300);
+    document.getElementById("show-more").textContent="show me more!";
+  }
+});
+
+// store slider
+
+  $('#slider-content').slick({
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      }
+    ]
+  });
+
+  // say hello button
+
+   $('#say-hello').click(function() {
+    $('#contactForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#contactForm");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+
+// fancybox
+
+
+  $(".fancybox").fancybox();
+
+});
